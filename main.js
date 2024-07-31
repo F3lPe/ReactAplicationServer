b0995b9eae97f8b0d5f7b6552e76459552d18fdc
Felipe do Nascimento <114013469+F3lPe@users.noreply.github.com>
2024-07-31 10:37:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port  = 3001
+const {v4: uuidv4} = require('uuid')
+app.use(express.json())
+
+const users = []
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).send('usuário não encontrado');
+    }
+});
+
+app.get('/users', (req, res) => {
+    if(users.length > 0) res.json(users)
+    else {
+        res.status(404).send('A lista de usuários está vazia')
+    }
+})
+
+
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: uuidv4(),
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(201).json(newUser).send('usuário criado com sucesso');
+});
+
+app.listen(port, () => {
+    console.log(`API listening at http://localhost:${port}`);
+});
